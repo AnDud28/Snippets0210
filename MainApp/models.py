721bb651ea1968be90eea6dc6931f44fd2188ef1
Vadim <vadimx30@gmail.com>
2023-10-13 19:30:30 +0300

    Add admin panel

--- MainApp/models.py
+++ MainApp/models.py
@@ -10,6 +10,9 @@ LANGS = (
 
 
 class Snippet(models.Model):
+    class Meta:
+        ordering = ['lang', 'creation_date']
+
     name = models.CharField(max_length=100)
     lang = models.CharField(max_length=30, choices=LANGS)
     code = models.TextField(max_length=5000)
@@ -18,4 +21,7 @@ class Snippet(models.Model):
     public = models.BooleanField(default=True)  # Если значение True -> публичный; если False -> приватный
 
     def __repr__(self):
+        return f'Snippet({self.id}, {self.name}, {self.lang}, {self.user}, {self.public})'
+    
+    def __str__(self):
         return f'Snippet({self.id}, {self.name}, {self.lang}, {self.user}, {self.public})'
